70c1efc97e8730618e0303f494d38aa030b4af47
耿子逾 <gengziyu@gengziyudeMacBook-Pro-7.local>
2023-04-08 01:05:41 -0400

    .

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pymongo
+import flask
 
 app = Flask(__name__)
 
@@ -24,4 +26,4 @@ def courses():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
