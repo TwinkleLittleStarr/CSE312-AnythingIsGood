b99cece4951f07d6bf481c347e331fcb6b677757
耿子逾 <gengziyu@gengziyudeMacBook-Pro-7.local>
2023-04-08 02:14:34 -0400

    .

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 import pymongo
 import flask
 
+mongo_client = pymongo.MongoClient("mongo")
+db = mongo_client["cse312"]
+
+user_collection = db['users']  # database to store the users
+
 app = Flask(__name__)
 
 
@@ -26,4 +31,4 @@ def courses():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)  # localhost:8080
