7f0b3ceef8d66a551113f08d1eb284a101b80b48
thqattr <tianruitu@gmail.com>
2023-04-08 22:59:01 -0400

    create course html and need add cookie to store user

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, redirect
 import pymongo
 import flask
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 mongo_client = pymongo.MongoClient("mongo")
 db = mongo_client["cse312"]
 
-user_collection = db['users']  # database to store the users
+user_collection = db['users']  # database to store the username and password
 
 app = Flask(__name__)
 
@@ -31,20 +31,25 @@ def index():  # homepage
 def register():
     # new users need to sign up, go to log in page after sign up
     if flask.request.method == 'POST':
-        Username = escape_text(flask.request.form['username'])
-        Password = flask.request.form['password']
+        username = escape_text(flask.request.form['username'])
+        password = flask.request.form['password']
+
         # username should not be empty, password should have at least 6 characters
-        if (not valid_text(Username) and len(Password) < 6):
+        if (not valid_text(username) and len(password) < 6):
             return render_template("register.html", registerStatus="Invalid input")
+
+        dic_username = list(user_collection.find({"username": username}))
+        if len(dic_username) != 0:  # Check if username is already in use
+            return render_template("register.html", registerStatus="Username already been used")
+
         else:
-            # hashed_password = generate_password_hash(Password)  # generate password in hash
+            hashed_password = generate_password_hash(password)  # generate password in hash for security
+
             # Insert new user into the database
-            print("username", Username)
-            user_collection.insert_one({"username": Username, "password": Password})
-            print(user_collection)
+            user_collection.insert_one({"username": username, "password": hashed_password})
+
             # Redirect to login page after registration
             return render_template("login.html")
-            # return redirect("/login")
     else:
         return render_template("register.html")
 
@@ -57,18 +62,18 @@ def login():
         password = flask.request.form['password']
         username_dic = user_collection.find_one({"username": username}, {"_id": 0})
         print(username_dic)
-
         if not username_dic:
             # If the username does not exist in the database, return to the login page
             return render_template("login.html", loginStatus="No Account, please register first")
         else:
-            if username_dic['password'] != password:
+            if not check_password_hash(username_dic['password'], password):
+                print(password)
+                print(check_password_hash(username_dic['password'], password))
                 # If password is incorrect, return to the login page
                 return render_template("login.html", loginStatus="Incorrect Password")
             else:
                 # If both username and password are correct, go to personal homepage
                 return render_template("index.html", user=username_dic)
-
     else:
         return render_template("login.html")
 
